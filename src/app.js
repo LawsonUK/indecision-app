b713a4e0c8e4271013fe4812d2aa92d46499b93c
LawsonUK <chrislawsonuk@hotmail.com>
2020-10-19 13:29:00 +0100

    Completed setting up webpack

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import subtract, { square, add } from './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/IndecisionApp'
 
-console.log('app.js is running!')
-console.log(square(4))
-console.log(add(2, 2))
-console.log(subtract(2, 1))
-
-import isSenior, { isAdult, canDrink } from './person'
-
-console.log(`Is an adult  ${isAdult(18)}`)
-console.log(`Can drink = ${canDrink(21)}`)
-console.log(`Is Senior = ${isSenior(64)}`)
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
